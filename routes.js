c0ad50e9b822d9fa74ea268d38a4a32cf0eeb4f1
antoniocrdns <gohan21.d@gmail.com>
2024-03-03 08:23:38 -0700

    Se añadió metodos asincronos de carga con try catch y un nuevo metodo de llamado por ID

--- routes.js
+++ routes.js
@@ -2,52 +2,83 @@ const express = require('express')
 const routes = express.Router()
 
 // --------- Consumir datos ------------
-routes.get('/', (req, res) => {
-    req.getConnection((err, conn) => {
-        if (err) return res.send(err)
-
-        conn.query('select * from eventos', (err, rows) => {
+routes.get('/', async (req, res) => {
+    try {
+        req.getConnection((err, conn) => {
             if (err) return res.send(err)
+            conn.query('select * from eventos', (err, rows) => {
+                if (err) return res.send(err)
 
-            res.json(rows)
+                res.json(rows)
+            })
         })
-    })
+    } catch (error) {
+        res.status(500);
+    }
+})
+
+// --------- Consumir dato ------------
+routes.get('/:id', async (req, res) => {
+    try {
+        req.getConnection((err, conn) => {
+            if (err) return res.send(err)
+            conn.query('select * from eventos where id = ?', [req.params.id], (err, rows) => {
+                if (err) return res.send(err)
+
+                res.json(rows)
+            })
+        })
+    } catch (error) {
+        res.status(500);
+    }
 })
 
 // --------- Insertar datos --------------
-routes.post('/', (req, res) => {
-    req.getConnection((err, conn) => {
-        if (err) return res.send(err)
-        conn.query('insert into eventos set ?', [req.body], (err, rows) => {
+routes.post('/', async (req, res) => {
+    try {
+        req.getConnection((err, conn) => {
             if (err) return res.send(err)
+            conn.query('insert into eventos set ?', [req.body], (err, rows) => {
+                if (err) return res.send(err)
 
-            res.send('evento añadido.')
+                res.send('evento añadido.')
+            })
         })
-    })
+    } catch (error) {
+        res.status(500);
+    }
 })
 
 // --------- Eliminar datos -------------
-routes.delete('/:id', (req, res) => {
-    req.getConnection((err, conn) => {
-        if (err) return res.send(err)
-        conn.query('delete from eventos where id = ?', [req.params.id], (err, rows) => {
+routes.delete('/:id', async (req, res) => {
+    try {
+        req.getConnection((err, conn) => {
             if (err) return res.send(err)
+            conn.query('delete from eventos where id = ?', [req.params.id], (err, rows) => {
+                if (err) return res.send(err)
 
-            res.send('evento eliminado.')
+                res.send('evento eliminado.')
+            })
         })
-    })
+    } catch (error) {
+        res.status(500);
+    }
 })
 
 // --------- Actualizar datos -------------
-routes.put('/:id', (req, res) => {
-    req.getConnection((err, conn) => {
-        if (err) return res.send(err)
-        conn.query('update eventos set ? where id = ?', [req.body, req.params.id], (err, rows) => {
+routes.put('/:id', async (req, res) => {
+    try {
+        req.getConnection((err, conn) => {
             if (err) return res.send(err)
+            conn.query('update eventos set ? where id = ?', [req.body, req.params.id], (err, rows) => {
+                if (err) return res.send(err)
 
-            res.send('evento actualizado.')
+                res.send('evento actualizado.')
+            })
         })
-    })
+    } catch (error) {
+        res.status(500);
+    }
 })
 
 
